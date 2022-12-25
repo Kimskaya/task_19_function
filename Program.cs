b30bb8ba7230ffba126759a923d9b22a91de286f
AnnaKim <annakim@inbox.ru>
2022-12-26 00:06:56 +0300

    task_19 completed

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿int Get5DigitNumber(string message)
+{
+    Console.WriteLine(message);
+    int AnyNumber = int.Parse(Console.ReadLine()!); 
+    return AnyNumber;
+}
+int AnyNumber= Get5DigitNumber("Input 5 digit  number");
+
+int FirstHalf = AnyNumber/1000;
+int SecondHalf = AnyNumber%100;
+
+int GetReversedNumber(int AnyNumber)
+   {
+        
+        int Rev = SecondHalf%10*10+SecondHalf/10;
+    
+        return Rev;
+   }
+     int ThreeDigitNumber = GetReversedNumber(AnyNumber);
+    if (ThreeDigitNumber == FirstHalf)
+    {
+     Console.WriteLine($"  Number {AnyNumber} is a palindrome");
+    }
+     else
+     {
+        Console.WriteLine($"  Number {AnyNumber} is not a palindrome");
+     }
+      
+
